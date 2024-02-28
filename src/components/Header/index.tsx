88f1e92ee3ac669d0852c48e5f5bce568abcfdf3
jean carllo <jeanrosete04@gmail.com>
2024-02-28 11:49:28 -0300

    finalizado o css do cabeçalho.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import { HeaderBar, Links, LinksItens, LinkCart } from './styles'
+
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
+const Header = () => {
+  return (
+    <HeaderBar>
+      <div>
+        <img src={logo} alt="Eplay" />
+        <nav>
+          <Links>
+            <LinksItens>
+              <a href="#">Categorias</a>
+            </LinksItens>
+            <LinksItens>
+              <a href="#">Novidades</a>
+            </LinksItens>
+            <LinksItens>
+              <a href="#">Promoções</a>
+            </LinksItens>
+          </Links>
+        </nav>
+      </div>
+      <LinkCart>
+        0 - produtos(s) <img src={carrinho} alt="imagem de um corrinho" />
+      </LinkCart>
+    </HeaderBar>
+  )
+}
+
+export default Header
--- src/styles.ts
+++ src/styles.ts
@@ -1,20 +1,29 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const cores = {
-  branca: '#333',
-  cinza: '#EEEEEE',
+export const cores = {
+  branca: '#eee',
+  cinza: '#333',
   verde: '#10AC84',
   preto: '#111'
 }
 
-const GlobalCss = createGlobalStyle`
+export const GlobalCss = createGlobalStyle`
 * {
-  border-size: border-box;
+  box-sizing: border-box;
   margin: 0;
+  padding: 0;
   font-family: Roboto, sans-serif;
+  list-style: none;
   }
 
   body {
-    background-color: ${cores.preto}
+    background-color: ${cores.preto};
+    color: ${cores.branca};
+    padding-top: 40px;
   }
+`
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 `
