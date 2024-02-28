0078cb06de949b30851607b9afd1f57f821feecc
jean carllo <jeanrosete04@gmail.com>
2024-02-28 18:56:10 -0300

    finalizado os Buttons/Tags e inserido os mesmo no Banner.

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,16 +2,26 @@ import { Imagem, Preco, Titulos } from './styles'
 
 import BannerImg from '../../assets/images/banner-homem-aranha.png'
 import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${BannerImg})` }}>
     <div className="container">
       <Tag size="big">Destaque do dia</Tag>
-      <Titulos>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulos>
-      <Preco>
-        De <span>R$ 250,00</span> <br />
-        por apenas R$ 99,00
-      </Preco>
+      <div>
+        <Titulos>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulos>
+        <Preco>
+          De <span>R$ 250,00</span> <br />
+          por apenas R$ 99,00
+        </Preco>
+      </div>
+      <Button
+        type="link"
+        to="/produto"
+        title="Clique aqui para aporveiar esta oferta"
+      >
+        Aproveitar
+      </Button>
     </div>
   </Imagem>
 )
